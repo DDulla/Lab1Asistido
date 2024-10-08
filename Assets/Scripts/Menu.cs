edbe91c976d5ce739e873f32c23d7ccb6f02e8e9
DDulla <yaperdilacabeza@outlook.com>
2024-10-08 06:08:29 -0500

    MenuCreated

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    public void PLAY()
+    {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QUIT()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}

58d6ed93fa058af211a32c7c26693b464aef6b7f
DDulla <yaperdilacabeza@outlook.com>
2024-10-08 07:32:03 -0500

    Scripts Set Up

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -11,6 +11,10 @@ public class Menu : MonoBehaviour
     {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void BACK()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 
     public void QUIT()
     {
---
+++ Assets/Scripts/ScriptableObjects/ColorShapeData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorShapeData", menuName = "ScriptableObjects/Game1/ColorShapeData")]
+public class ColorShapeData : ScriptableObject
+{
+
+}
